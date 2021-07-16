e093f9b018567a5cf189a2ce40f9535946a2e844
Guilherme-Ferreti <guiferretis@gmail.com>
2021-07-16 17:19:08 -0300

    added database migrations

---
+++ database/migrations/2021_07_16_161700_create_addresses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->char('cep', 9);
+            $table->string('street');
+            $table->string('number', 6);
+            $table->string('neighborhood');
+            $table->string('city');
+            $table->string('state');
+            $table->foreignId('order_id')->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}

ccac7aefbc41a1df766b5048a03d1fff6c36969e
Guilherme-Ferreti <guiferretis@gmail.com>
2021-07-18 19:15:11 -0300

    DatabaseSeeder improved

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,12 +17,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $products = Product::factory(15)->has(Supplier::factory(2))->create();
+        $products = Product::factory(15)->has(Supplier::factory(rand(1, 3)))->create();
         
         $orders = Order::factory(5)->has(Address::factory())->create();
 
         foreach ($orders as $order) {
-            $products->random(3)->each(fn ($product) =>
+            $products->random(rand(1, 5))->each(fn ($product) =>
                 $order->products()->attach($product->id, [
                     'quantity' => rand(1, 5),
                     'selling_price' => rand(1, 5000) / 10,

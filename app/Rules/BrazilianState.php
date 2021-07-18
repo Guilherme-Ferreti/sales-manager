56bd49df7405784162e3a03b144b737f811f6909
Guilherme-Ferreti <guiferretis@gmail.com>
2021-07-18 15:21:24 -0300

    added initials to Brazilian states array

--- app/Rules/BrazilianState.php
+++ app/Rules/BrazilianState.php
@@ -3,37 +3,38 @@
 namespace App\Rules;
 
 use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Support\Arr;
 
 class BrazilianState implements Rule
 {
     const STATES = [
-        'Acre',               
-        'Alagoas',            
-        'Amapá',              
-        'Amazonas',           
-        'Bahia' ,             
-        'Ceará',              
-        'Distrito Federal' ,  
-        'Espírito Santo',     
-        'Goiás',              
-        'Maranhão',           
-        'Mato Grosso',        
-        'Mato Grosso do Sul', 
-        'Minas Gerais',       
-        'Pará',               
-        'Paraíba',            
-        'Paraná',             
-        'Pernambuco',         
-        'Piauí',              
-        'Rio de Janeiro',     
-        'Rio Grande do Norte',
-        'Rio Grande do Sul',  
-        'Rondônia',           
-        'Roraima',            
-        'Santa Catarina',     
-        'São Paulo',          
-        'Sergipe',            
-        'Tocantins',          
+        ['name' => 'Acre',                  'initials' => 'AC'],
+        ['name' => 'Alagoas',               'initials' => 'AL'],
+        ['name' => 'Amapá',                 'initials' => 'AP'],
+        ['name' => 'Amazonas',              'initials' => 'AM'],
+        ['name' => 'Bahia' ,                'initials' => 'BA'],
+        ['name' => 'Ceará',                 'initials' => 'CE'],
+        ['name' => 'Distrito Federal' ,     'initials' => 'DF'],
+        ['name' => 'Espírito Santo',        'initials' => 'ES'],
+        ['name' => 'Goiás',                 'initials' => 'GO'],
+        ['name' => 'Maranhão',              'initials' => 'MA'],
+        ['name' => 'Mato Grosso',           'initials' => 'MT'],
+        ['name' => 'Mato Grosso do Sul',    'initials' => 'MS'],
+        ['name' => 'Minas Gerais',          'initials' => 'MG'],
+        ['name' => 'Pará',                  'initials' => 'PA'],
+        ['name' => 'Paraíba',               'initials' => 'PB'],
+        ['name' => 'Paraná',                'initials' => 'PR'],
+        ['name' => 'Pernambuco',            'initials' => 'PE'],
+        ['name' => 'Piauí',                 'initials' => 'PI'],
+        ['name' => 'Rio de Janeiro',        'initials' => 'RJ'],
+        ['name' => 'Rio Grande do Norte',   'initials' => 'RN'],
+        ['name' => 'Rio Grande do Sul',     'initials' => 'RS'],
+        ['name' => 'Rondônia',              'initials' => 'RO'],
+        ['name' => 'Roraima',               'initials' => 'RR'],
+        ['name' => 'Santa Catarina',        'initials' => 'SC'],
+        ['name' => 'São Paulo',             'initials' => 'SP'],
+        ['name' => 'Sergipe',               'initials' => 'SE'],
+        ['name' => 'Tocantins',             'initials' => 'TO'],
     ];
 
     /**
@@ -45,7 +46,7 @@ class BrazilianState implements Rule
      */
     public function passes($attribute, $value)
     {
-        return in_array($value, Self::STATES);
+        return in_array($value, Arr::pluck(Self::STATES, 'initials'));
     }
 
     /**
